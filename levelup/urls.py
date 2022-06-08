228f65223760ea633c34ce84ce04857c8d9c28ae
Chaneice Jackson <c.jackson0126@gmail.com>
2022-06-07 21:28:38 -0500

    chapter 6 completed

--- levelup/urls.py
+++ levelup/urls.py
@@ -17,6 +17,15 @@ from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
 from levelupapi.views import register_user, login_user
+from rest_framework import routers
+from levelupapi.views import GameTypeView
+from levelupapi.views import EventView
+from levelupapi.views import GameView
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'gametypes', GameTypeView, 'gametype')
+router.register(r'events', EventView, 'event')
+router.register(r'games', GameView, 'game')
 
 urlpatterns = [
     # Requests to https://localhost:8000/register will be routed to the register_user function
@@ -24,4 +33,5 @@ urlpatterns = [
     # Requests to http://localhost:8--/login will be routed to the login_user function
     path('login', login_user),
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
---
+++ levelupapi/views/event.py
@@ -0,0 +1,42 @@
+"""View module for handling requests about events"""
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import  serializers, status
+from levelupapi.models import Event
+
+class EventView(ViewSet):
+    """Level up event view"""
+    def retrieve(self, request, pk):
+        """Handle GET requests for single event
+
+        Returns:
+            Response -- JSON serialized event
+        """
+        event = Event.objects.get(pk=pk)
+        serializer = EventSerializer(event)
+        return Response(serializer.data)
+
+    def list(self, request):
+        """Handle GET requests to get all event
+
+        Returns:
+            Response -- JSON serialized list of event
+        """
+        events = Event.objects.all()
+
+        # Add in the next 3 lines
+        game = request.query_params.get('type', None)
+        if game is not None:
+            events = events.filter(game_id=game)
+
+        serializer = EventSerializer(events, many=True)
+        return Response(serializer.data)
+
+class EventSerializer(serializers.ModelSerializer):
+    """JSON serializer for game types
+    """
+    class Meta:
+        model = Event
+        fields = ('id', 'game', 'description', 'date_time', 'organizer')
+        depth = 2
